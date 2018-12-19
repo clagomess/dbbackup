16fac60bc81082ce4377a10d90c206189cefc8d2
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2018-12-19 18:19:20 -0200

    migration test

---
+++ src/test/java/br/dbbackup/main/MainMysqlTest.java
@@ -0,0 +1,125 @@
+package br.dbbackup.main;
+
+import br.dbbackup.util.TestUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+
+@Slf4j
+public class MainMysqlTest {
+    @Test
+    public void dump() throws Throwable {
+        String workdir = TestUtil.getNewWorkDir();
+
+        // DUMP
+        Main.main(new String[]{
+                "-db", "MYSQL",
+                "-lob", "1",
+                "-ope", "GET",
+                "-url", TestUtil.URL_MYSQL,
+                "-user", TestUtil.USER_MYSQL,
+                "-pass", TestUtil.PASS_MYSQL,
+                "-schema", TestUtil.SCHEMA_MYSQL,
+                "-workdir", workdir,
+                "-table", "tbl_dbbackup"
+        });
+
+        Assert.assertTrue((new File(String.format("%s/%s.tbl_dbbackup.sql", workdir, TestUtil.SCHEMA_MYSQL))).isFile());
+
+        // PUMP
+        Main.main(new String[]{
+                "-db", "MYSQL",
+                "-lob", "1",
+                "-ope", "PUT",
+                "-url", TestUtil.URL_MYSQL,
+                "-user", TestUtil.USER_MYSQL,
+                "-pass", TestUtil.PASS_MYSQL,
+                "-schema", TestUtil.SCHEMA_MYSQL,
+                "-workdir", workdir
+        });
+    }
+
+    @Test
+    public void pumpOracle() throws Throwable {
+        String workdir = TestUtil.getNewWorkDir();
+
+        Main.main(new String[]{
+                "-db", "MYSQL",
+                "-lob", "1",
+                "-ope", "GET",
+                "-url", TestUtil.URL_MYSQL,
+                "-user", TestUtil.USER_MYSQL,
+                "-pass", TestUtil.PASS_MYSQL,
+                "-schema", TestUtil.SCHEMA_MYSQL,
+                "-workdir", workdir,
+                "-table", "tbl_dbbackup",
+                "-dump_format", "ORACLE"
+        });
+
+        File backupFile = new File(String.format("%s/%s.tbl_dbbackup.sql", workdir, TestUtil.SCHEMA_MYSQL));
+
+        String dml = new String(Files.readAllBytes(backupFile.toPath()));
+        dml = dml.replace("tbl_dbbackup", "tbl_dbbackup_mysql");
+        dml = dml.replace(TestUtil.SCHEMA_MYSQL, TestUtil.SCHEMA_ORACLE);
+        Files.write(backupFile.toPath(), dml.getBytes());
+
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "PUT",
+                "-url", TestUtil.URL_ORACLE,
+                "-user", TestUtil.USER_ORACLE,
+                "-pass", TestUtil.PASS_ORACLE,
+                "-schema", TestUtil.SCHEMA_ORACLE,
+                "-workdir", workdir
+        });
+    }
+
+    @Test
+    public void pumpPostgresql() throws Throwable {
+        String workdir = TestUtil.getNewWorkDir();
+
+        Main.main(new String[]{
+                "-db", "MYSQL",
+                "-lob", "1",
+                "-ope", "GET",
+                "-url", TestUtil.URL_MYSQL,
+                "-user", TestUtil.USER_MYSQL,
+                "-pass", TestUtil.PASS_MYSQL,
+                "-schema", TestUtil.SCHEMA_MYSQL,
+                "-workdir", workdir,
+                "-table", "tbl_dbbackup",
+                "-dump_format", "POSTGRESQL"
+        });
+
+        File backupFile = new File(String.format("%s/%s.tbl_dbbackup.sql", workdir, TestUtil.SCHEMA_MYSQL));
+
+        String dml = new String(Files.readAllBytes(backupFile.toPath()));
+        dml = dml.replace("tbl_dbbackup", "tbl_dbbackup_mysql");
+        dml = dml.replace(TestUtil.SCHEMA_MYSQL, TestUtil.SCHEMA_POSTGRESQL);
+        Files.write(backupFile.toPath(), dml.getBytes());
+
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "PUT",
+                "-url", TestUtil.URL_POSTGRESQL,
+                "-user", TestUtil.USER_POSTGRESQL,
+                "-pass", TestUtil.PASS_POSTGRESQL,
+                "-schema", TestUtil.SCHEMA_POSTGRESQL,
+                "-workdir", workdir
+        });
+    }
+
+    @After
+    public void after() throws Exception {
+        //@TODO: implements
+        /*for (File dir : TestUtil.workdirs){
+            FileUtils.deleteDirectory(dir);
+        }*/
+    }
+}
---
+++ src/test/java/br/dbbackup/main/MainOracleTest.java
@@ -0,0 +1,125 @@
+package br.dbbackup.main;
+
+import br.dbbackup.util.TestUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+
+@Slf4j
+public class MainOracleTest {
+    @Test
+    public void dump() throws Throwable {
+        String workdir = TestUtil.getNewWorkDir();
+
+        // DUMP
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "GET",
+                "-url", TestUtil.URL_ORACLE,
+                "-user", TestUtil.USER_ORACLE,
+                "-pass", TestUtil.PASS_ORACLE,
+                "-schema", TestUtil.SCHEMA_ORACLE,
+                "-workdir", workdir,
+                "-table", "TBL_DBBACKUP"
+        });
+
+        Assert.assertTrue((new File(String.format("%s/%s.tbl_dbbackup.sql", workdir, TestUtil.SCHEMA_ORACLE))).isFile());
+
+        // PUMP
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "PUT",
+                "-url", TestUtil.URL_ORACLE,
+                "-user", TestUtil.USER_ORACLE,
+                "-pass", TestUtil.PASS_ORACLE,
+                "-schema", TestUtil.SCHEMA_ORACLE,
+                "-workdir", workdir
+        });
+    }
+
+    @Test
+    public void pumpMysql() throws Throwable {
+        String workdir = TestUtil.getNewWorkDir();
+
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "GET",
+                "-url", TestUtil.URL_ORACLE,
+                "-user", TestUtil.USER_ORACLE,
+                "-pass", TestUtil.PASS_ORACLE,
+                "-schema", TestUtil.SCHEMA_ORACLE,
+                "-workdir", workdir,
+                "-table", "TBL_DBBACKUP",
+                "-dump_format", "MYSQL"
+        });
+
+        File backupFile = new File(String.format("%s/%s.tbl_dbbackup.sql", workdir, TestUtil.SCHEMA_ORACLE));
+
+        String dml = new String(Files.readAllBytes(backupFile.toPath()));
+        dml = dml.replace("TBL_DBBACKUP", "tbl_dbbackup_oracle");
+        dml = dml.replace(TestUtil.SCHEMA_ORACLE, TestUtil.SCHEMA_MYSQL);
+        Files.write(backupFile.toPath(), dml.getBytes());
+
+        Main.main(new String[]{
+                "-db", "MYSQL",
+                "-lob", "1",
+                "-ope", "PUT",
+                "-url", TestUtil.URL_MYSQL,
+                "-user", TestUtil.USER_MYSQL,
+                "-pass", TestUtil.PASS_MYSQL,
+                "-schema", TestUtil.SCHEMA_MYSQL,
+                "-workdir", workdir
+        });
+    }
+
+    @Test
+    public void pumpPostgresql() throws Throwable {
+        String workdir = TestUtil.getNewWorkDir();
+
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "GET",
+                "-url", TestUtil.URL_ORACLE,
+                "-user", TestUtil.USER_ORACLE,
+                "-pass", TestUtil.PASS_ORACLE,
+                "-schema", TestUtil.SCHEMA_ORACLE,
+                "-workdir", workdir,
+                "-table", "TBL_DBBACKUP",
+                "-dump_format", "POSTGRESQL"
+        });
+
+        File backupFile = new File(String.format("%s/%s.tbl_dbbackup.sql", workdir, TestUtil.SCHEMA_ORACLE));
+
+        String dml = new String(Files.readAllBytes(backupFile.toPath()));
+        dml = dml.replace("TBL_DBBACKUP", "tbl_dbbackup_oracle");
+        dml = dml.replace(TestUtil.SCHEMA_ORACLE, TestUtil.SCHEMA_POSTGRESQL);
+        Files.write(backupFile.toPath(), dml.getBytes());
+
+        Main.main(new String[]{
+                "-db", "ORACLE",
+                "-lob", "1",
+                "-ope", "PUT",
+                "-url", TestUtil.URL_POSTGRESQL,
+                "-user", TestUtil.USER_POSTGRESQL,
+                "-pass", TestUtil.PASS_POSTGRESQL,
+                "-schema", TestUtil.SCHEMA_POSTGRESQL,
+                "-workdir", workdir
+        });
+    }
+
+    @After
+    public void after() throws Exception {
+        //@TODO: implements
+        /*for (File dir : TestUtil.workdirs){
+            FileUtils.deleteDirectory(dir);
+        }*/
+    }
+}
