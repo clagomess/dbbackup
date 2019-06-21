2d453a203de4c4ab2621f3f4093f4f95ba08b54d
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-06-21 17:23:13 -0300

    sqlite - fix postgresql

--- src/main/resources/sql/postgresql_tabcolumns.sql
+++ src/main/resources/sql/postgresql_tabcolumns.sql
@@ -4,6 +4,6 @@ JOIN information_schema.tables t
   on t.table_catalog = c.table_catalog
   AND t.table_schema = c.table_schema
   AND t.table_name = c.table_name
-where c.table_schema = '%s' AND t.table_type <> 'VIEW'
+where c.table_schema = :TABLE_SCHEMA AND t.table_type <> 'VIEW'
 AND (:TABLE_NAME IS NULL OR c.table_name in (:TABLE_NAME_AR))
 order by c.table_name, c.ordinal_position
