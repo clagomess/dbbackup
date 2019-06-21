696dbf5be71fe2cb59c9e5f0b2a734e4589d5f3f
CLAUDIO GOMES <claudio.silva@fnde.gov.br>
2019-06-21 18:19:47 -0300

    sqlite - fix query

--- src/main/resources/sql/sqlite_tabcolumns.sql
+++ src/main/resources/sql/sqlite_tabcolumns.sql
@@ -2,4 +2,5 @@ select sm.name "table_name", pti.name "column_name", pti.type "data_type"
 from sqlite_master sm
 join pragma_table_info(sm.name) pti
 where sm.type = 'table'
+AND (:TABLE_NAME IS NULL OR sm.name in (:TABLE_NAME_AR))
 order by sm.name, pti.cid
